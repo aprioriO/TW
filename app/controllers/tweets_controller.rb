ff41f35596152ead7f1f44f64bb4b6624bc99ce8
Olena Hulakova <ohulakova@gmail.com>
2020-05-16 22:56:14 +0100

    Add show action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
+
   def index
 
+  end
+
+  def show
+    @tweet = {id: params[:id]}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-   get '/tweets', to: 'tweets#index'
+   get '/tweets',      to: 'tweets#index'
+   get '/tweets/:id',  to: 'tweets#show'
+
 end
