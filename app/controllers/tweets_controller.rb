7ee182ea91dfb911d009c8c79e1ee45932f149a5
Olena Hulakova <ohulakova@gmail.com>
2020-05-16 23:17:15 +0100

    Add update action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,17 @@
 class TweetsController < ApplicationController
 
   def index
-
   end
 
   def show
     @tweet = {id: params[:id]}
+  end
+
+  def edit
+    @tweet = {id: params[:id], message: "Some tweet"}
+  end
+
+  def update
+    redirect_to action: :show, id: params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
    get '/tweets',      to: 'tweets#index'
    get '/tweets/:id',  to: 'tweets#show'
 
+   get '/tweets/:id/edit', to: 'tweets#edit'
+   post '/tweets/:id/edit', to: 'tweets#update'
+
+
+
 end
