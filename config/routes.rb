d06956eb657dee0157867086ef2161ee7012fd2f
Olena Hulakova <ohulakova@gmail.com>
2020-05-16 23:57:17 +0100

    Use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-   get '/tweets',      to: 'tweets#index'
-   get '/tweets/new', to: 'tweets#new'
-   post '/tweets/create', to: 'tweets#create'
+   # get '/tweets',      to: 'tweets#index'
+   # get '/tweets/new', to: 'tweets#new'
+   # post '/tweets', to: 'tweets#create'
+   # get '/tweets/:id',  to: 'tweets#show'
+   # get '/tweets/:id/edit', to: 'tweets#edit'
+   # PATCH '/tweets/:id', to: 'tweets#update'
+   # DELETE '/tweets/:id/', to: 'tweets#destroy'
 
-   get '/tweets/:id',  to: 'tweets#show'
+   resources :tweets
 
-   get '/tweets/:id/edit', to: 'tweets#edit'
-   post '/tweets/:id/edit', to: 'tweets#update'
-
-  post '/tweets/:id/delete', to: 'tweets#destroy'
-
-
+# GET    /tweets            tweets#index  +
+# POST   /tweets            tweets#create +
+# GET    /tweets/new        tweets#new    +
+# GET    /tweets/:id/edit   tweets#edit   +
+# GET    /tweets/:id        tweets#show   +
+# PATCH  /tweets/:id        tweets#update +
+# PUT    /tweets/:id        tweets#update
+# DELETE /tweets/:id        tweets#destroy+
 end
